d356d6b8443e3e5b14200fe991861a5a8f96e78a
ngibson1129 <31421509+ngibson1129@users.noreply.github.com>
2017-09-28 13:26:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function()
+{
+     var circle =  document.getElementsByClassName("circle");
+     
+     
+     function CreateDiv(circle)
+     {
+          var div = document.createElement("id");
+          object.id = Id;
+          object.addEventListener("click");
+          return div;
+     }
+}
