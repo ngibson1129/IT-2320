84aadcf55460f1bdc9eaf723e1f9092bd59908bb
ngibson1129 <31421509+ngibson1129@users.noreply.github.com>
2017-10-09 18:34:36 -0400

    Add files via upload

---
+++ MyChheckerBoard2/App_Start/Home.js
@@ -0,0 +1,20 @@
+﻿$(".cell").on("click", function () {
+    $(this).css({ backgroundColor: "red" });
+});
+
+
+$(document).ready(function () {
+    var cells = $(".cell");
+    var colorCount = 0;
+
+    for (var i = 0; i < cells.length; i++) {
+
+        var cell = $(cells[i]);
+        var isDark = colorCount % 2 === 0;
+        var isNextRow = (i + 1) % 8 === 0;
+        colorCount += isNextRow ? 2 : 1;
+        cell.css("background-color", isDark ? "orange" : "navy");
+
+    }
+});
+
---
+++ MyChheckerBoard2/Models/HomeModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyChheckerBoard2.Models
+{
+    public class Home
+    {
+    }
+}
