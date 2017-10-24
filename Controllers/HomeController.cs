c10434f4219e28828e62c0124d6f951f9522e8c8
ngibson1129 <31421509+ngibson1129@users.noreply.github.com>
2017-10-23 23:28:58 -0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using CavsAssignment.Extentions;
+using CavsAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CavsAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [AllowCrossSiteJson]
+        public JsonResult GetPlayerInformation(HomeModel model)
+        {
+            var response = new ResponseModel();
+            response.PlayerNumber = model.PlayerNumber;
+
+            if (model.PlayerNumber == 23)
+            {
+                response.PlayerName = "Lebron James";
+            }
+            else if (model.PlayerNumber == 2)
+            {
+                response.PlayerName = "Kyrie Irving";
+            }
+            else if (model.PlayerNumber == 17)
+            {
+                response.PlayerName = "Anderson Varejão";
+            }
+            else
+            {
+                response.PlayerName = "Player Not Found";
+            }
+
+            return new JsonResult().SerializeObject(response);
+        }
+    }
+}
+    
